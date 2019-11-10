164f0ccf828748ebdb9ad9e45a9c283192417dba
hw-2030 <hydenw2030@gmail.com>
2019-11-10 17:22:02 -0500

    Added prop placement script stubs

---
+++ flint_westwood_active/Assets/Scripts/Prop Placement/PropManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropManager : MonoBehaviour
+{
+    private PropPlacementHandler _propPlacementHandler;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
