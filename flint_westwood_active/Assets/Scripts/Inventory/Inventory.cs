02fd26b8fdbd48a55ebc22c340cf4d7a4be2b532
Wyatt Joyner <wyatt.joyner@practicalcode.us>
2019-11-09 21:51:05 -0500

    added font, made inventory item, basic stubs

--- flint_westwood_active/Assets/Scripts/Inventory/Inventory.cs
+++ flint_westwood_active/Assets/Scripts/Inventory/Inventory.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public static Inventory Instance;
+
+    Transform weapon_content, placeable_content;
+
+    List<GameObject> items = new List<GameObject>();
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if (Instance && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        weapon_content = transform.FindDeepChild("WeaponContent");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddItem(GameObject item)
     {
-        
+        GameObject spawned_item = Instantiate(item, transform.position, Quaternion.identity);
+        spawned_item.transform.parent = weapon_content;
+        items.Add(spawned_item);
+    }
+
+    public void RemoveItem(GameObject item)
+    {
+        items.Remove(item);
+        Destroy(item);
     }
 }
--- flint_westwood_active/Assets/Scripts/Inventory/InventoryItem.cs
+++ flint_westwood_active/Assets/Scripts/Inventory/InventoryItem.cs
@@ -6,13 +6,12 @@ public class InventoryItem : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
     }
 
     // Update is called once per frame
-    void Update()
+    public void OnClick()
     {
-        
+        // tell the workbench to select me!
     }
 }
